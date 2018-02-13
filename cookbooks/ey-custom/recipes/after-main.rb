7d265a5c0244b4c9e6ab6f46648c1d90bc4e9ba0
Staslo <stivoadarzvit@gmail.com>
2018-02-13 11:58:17 +0200

    enable recipes

--- cookbooks/ey-custom/recipes/after-main.rb
+++ cookbooks/ey-custom/recipes/after-main.rb
@@ -1,3 +1,8 @@
 # Include your desired recipe(s) to run after the completion of the main execution phase
 #
 # include_recipe "my_cookbook::my_recipe"
+
+include_recipe 'thinking-sphinx-3'
+include_recipe 'redis'
+include_recipe 'sidekiq'
+require_recipe 'memcached'
